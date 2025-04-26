1117a60bfbae3f6a1faea9193b66e341dcee72bf
MeBigSnail <81766000+MeBigSnail@users.noreply.github.com>
2025-04-26 20:33:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyDQjt5Vqyp7zs5ppnZasuNHKkGg6DTGFQU",
   authDomain: "furniturefuture-ed302.firebaseapp.com",
   projectId: "furniturefuture-ed302",
-  storageBucket: "furniturefuture-ed302.firebasestorage.app",
+  storageBucket: "furniturefuture-ed302.appspot.com", // <-- corect
   messagingSenderId: "746220689658",
   appId: "1:746220689658:web:2fc5949203b9de6fb11143",
   measurementId: "G-GBMPN4YJ7Y"
 };
+
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
