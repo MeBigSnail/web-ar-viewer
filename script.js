f2bab9f3f307c8ba09fd51948ea4fa293aad0b56
MeBigSnail <81766000+MeBigSnail@users.noreply.github.com>
2025-04-26 20:35:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDQjt5Vqyp7zs5ppnZasuNHKkGg6DTGFQU",
   authDomain: "furniturefuture-ed302.firebaseapp.com",
   projectId: "furniturefuture-ed302",
-  storageBucket: "furniturefuture-ed302.appspot.com", // <-- corect
+  storageBucket: "furniturefuture-ed302.firebasestorage.app",
   messagingSenderId: "746220689658",
   appId: "1:746220689658:web:2fc5949203b9de6fb11143",
   measurementId: "G-GBMPN4YJ7Y"
